fb92ddf3729455bfcf0890da20e3be17875dc103
Daniil Gavrilov <dany.gavrik@mail.ru>
2022-08-09 16:01:27 +0700

    Add entity

---
+++ src/main/java/com/rest/api/entity/Student.java
@@ -0,0 +1,26 @@
+package com.rest.api.entity;
+
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@Table(name = "students")
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "firstname")
+    private String firstname;
+
+    @Column(name = "lastname")
+    private String lastname;
+
+    @Column(name = "course")
+    private int course;
+}
