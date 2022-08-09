89ceb36b99945bc5524c77ab531c234d7cc4d95e
Daniil Gavrilov <dany.gavrik@mail.ru>
2022-08-09 16:02:25 +0700

    Init database

---
+++ src/main/resources/data.sql
@@ -0,0 +1,46 @@
+INSERT INTO students (id, course, firstname, lastname)
+VALUES ('1', '2', 'Richard', 'Reed');
+
+INSERT INTO students (id, course, firstname, lastname)
+VALUES ('2', '1', 'Aiden', 'Anderson');
+
+INSERT INTO students (id, course, firstname, lastname)
+VALUES ('3', '1', 'Luis', 'Robinson');
+
+INSERT INTO students (id, course, firstname, lastname)
+VALUES ('4', '3', 'Christopher', 'Brooks');
+
+INSERT INTO students (id, course, firstname, lastname)
+VALUES ('5', '4', 'Gavin', 'Griffin');
+
+INSERT INTO students (id, course, firstname, lastname)
+VALUES ('6', '2', 'Bailey', 'Kelly');
+
+INSERT INTO students (id, course, firstname, lastname)
+VALUES ('7', '1', 'Anna', 'Thompson');
+
+INSERT INTO students (id, course, firstname, lastname)
+VALUES ('8', '1', 'Danielle', 'Russell');
+
+INSERT INTO students (id, course, firstname, lastname)
+VALUES ('9', '3', 'Michelle', 'Cooper');
+
+INSERT INTO students (id, course, firstname, lastname)
+VALUES ('10', '4', 'Melissa', 'Clark');
+
+INSERT INTO students (id, course, firstname, lastname)
+VALUES ('11', '2', 'Katelyn', 'Washington');
+
+INSERT INTO students (id, course, firstname, lastname)
+VALUES ('12', '1', 'Cody', 'Patterson');
+
+INSERT INTO students (id, course, firstname, lastname)
+VALUES ('13', '1', 'Luis', 'Morgan');
+
+INSERT INTO students (id, course, firstname, lastname)
+VALUES ('14', '3', 'Nathan', 'Foster');
+
+INSERT INTO students (id, course, firstname, lastname)
+VALUES ('15', '4', 'Brandon', 'Watson');
+
+
