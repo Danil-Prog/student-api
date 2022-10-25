245b754360d53207dd1ec5fd4f368e694ce769d1
Daniil Gavrilov <dany.gavrik@mail.ru>
2022-10-25 15:31:24 +0700

    Rename UserAdvice to ControllerExceptionHandler

--- src/main/java/com/rest/api/exception/ControllerExceptionHandler.java
+++ src/main/java/com/rest/api/exception/ControllerExceptionHandler.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
-public class UserAdvice {
+public class ControllerExceptionHandler {
 
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<Response> handleException(ResourceNotFoundException exception) {
